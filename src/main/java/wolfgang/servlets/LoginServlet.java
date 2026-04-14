e03124987328442b092f4beeef256e386613d8a6
Adam <ahaddadi0203@gmail.com>
2026-04-14 21:33:54 +0200

    added register and login pages

---
+++ src/main/java/wolfgang/servlets/LoginServlet.java
@@ -0,0 +1,50 @@
+package wolfgang.servlets;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import wolfgang.entities.User;
+import wolfgang.repositories.UserRepository;
+import wolfgang.utils.FlashMessageUtils;
+
+import java.io.IOException;
+
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("/WEB-INF/login.jsp").forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession();
+
+        String username = req.getParameter("username");
+        String password = req.getParameter("password");
+
+        if (username == null || username.isBlank()) {
+            req.setAttribute("error", "Veuillez entrer un nom d'utilisateur");
+            req.getRequestDispatcher("/WEB-INF/login.jsp").forward(req, resp);
+            return;
+        }
+        if (password == null || password.isBlank()) {
+            req.setAttribute("error", "Veuillez entrer un mot de passe");
+            req.getRequestDispatcher("/WEB-INF/login.jsp").forward(req, resp);
+            return;
+        }
+
+        User user = UserRepository.authenticate(username, password);
+        if (user != null) {
+            session.setAttribute("user", user);
+            resp.sendRedirect(req.getContextPath() + "/");
+        } else {
+            FlashMessageUtils.setFlash(req, "error", "Nom d'utilisateur ou mot de passe invalide.");
+            resp.sendRedirect(req.getContextPath() + "/login");
+        }
+    }
+}
---
+++ src/main/webapp/WEB-INF/register.jsp
@@ -0,0 +1,9 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>S'inscrire</title>
+</head>
+<body>
+  <h1>S'inscrire</h1>
+</body>
+</html>
