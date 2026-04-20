cbd760d99742536465321c74d2f178b7102d42d3
Adam <arsegore@ikmail.com>
2026-04-20 19:18:54 +0200

    removed CompositionMember and added other composition-related repositories

---
+++ src/main/java/wolfgang/repositories/InstrumentRepository.java
@@ -0,0 +1,75 @@
+package wolfgang.repositories;
+
+import wolfgang.config.DatabaseConfig;
+import wolfgang.models.Instrument;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class InstrumentRepository {
+
+    /**
+     * @param id
+     * @return L'instrument correspondant ou null
+     */
+    public Instrument findById(int id) {
+        Instrument instrument = null;
+        String sql = """
+					SELECT *
+					FROM instruments
+					WHERE id = ?;
+					""";
+
+        try (
+                Connection con = DriverManager.getConnection(
+                        DatabaseConfig.DB_URL,
+                        DatabaseConfig.DB_LOGIN,
+                        DatabaseConfig.DB_PASSWD);
+                PreparedStatement stmt = con.prepareStatement(sql);
+        ) {
+            stmt.setInt(1, id);
+            ResultSet rs = stmt.executeQuery();
+
+            if (rs.next()) {
+                instrument = new Instrument(
+                        rs.getInt("id"),
+                        rs.getString("name")
+                );
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return instrument;
+    }
+
+    /**
+     * @return La liste de tous les instruments
+     */
+    public List<Instrument> findAll() {
+        List<Instrument> instruments = new ArrayList<>();
+        String sql = "SELECT * FROM instruments;";
+
+        try (
+                Connection con = DriverManager.getConnection(
+                        DatabaseConfig.DB_URL,
+                        DatabaseConfig.DB_LOGIN,
+                        DatabaseConfig.DB_PASSWD);
+                PreparedStatement stmt = con.prepareStatement(sql);
+        ) {
+            ResultSet rs = stmt.executeQuery();
+
+            while (rs.next()) {
+                instruments.add(new Instrument(
+                        rs.getInt("id"),
+                        rs.getString("name")
+                ));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return instruments;
+    }
+}
