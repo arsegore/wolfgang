6da6f043325ccb0ec4b4458ad02813fa53e4e007
PelouxL <Peloux.louis@gmail.com>
2026-05-03 11:14:36 +0200

    wip

--- src/main/java/wolfgang/servlets/RegisterCompServlet.java
+++ src/main/java/wolfgang/servlets/RegisterCompServlet.java
@@ -7,7 +7,11 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import wolfgang.config.DatabaseConfig;
+import wolfgang.daos.CompositionDAO;
 import wolfgang.daos.UserDAO;
+import wolfgang.models.Composition;
+import wolfgang.models.User;
+import wolfgang.utils.FlashMessageUtils;
 
 import java.io.IOException;
 
@@ -31,9 +35,36 @@ public class RegisterCompServlet extends HttpServlet {
         req.getRequestDispatcher("/WEB-INF/registerComp.jsp").forward(req, resp);
     }
 
-    protected void doPost(HttpServletRequest request, HttpServletResponse response){
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession();
+
+        User user = (User) session.getAttribute("user");
+        String nameComp = req.getParameter("nameComp");
+        int tempo = Integer.parseInt(req.getParameter("tempo"));
+        String accessType = req.getParameter("accessType");
+
+        if(nameComp == null || nameComp.isBlank()) {
+            FlashMessageUtils.setFlash(req, "error", "Veuillez entrer un nom de composition");
+            req.getRequestDispatcher("/WEB-INF/registerComp.jsp").forward(req, resp);
+            return;
+        }
+
+        if(CompositionDAO.create(new Composition(
+                nameComp,
+                tempo,
+                accessType,
+                user
+        ))){
+            FlashMessageUtils.setFlash(req, "success", "Inscription réussie.");
+            resp.sendRedirect(req.getContextPath() + "/login");
+        } else {
+            req.setAttribute("error", "Erreur d'inscription, veuillez réessayer.");
+            req.getRequestDispatcher("/WEB-INF/register.jsp").forward(req, resp);
+        }
 
     }
 
 
 }
+
+
