35fd646da0b2547d7bbe473b2b7bdbb6831d4e36
DamienGrjn <your_email@abc.example>
2026-05-14 20:05:12 +0200

    Ajout page liste d'amis

---
+++ src/main/java/wolfgang/models/Friendship.java
@@ -0,0 +1,22 @@
+package wolfgang.models;
+
+import java.time.LocalDateTime;
+
+public class Friendship {
+
+    private User friend;
+    private LocalDateTime friendsSince;
+
+    public Friendship(User friend, LocalDateTime friendsSince) {
+        this.friend = friend;
+        this.friendsSince = friendsSince;
+    }
+
+    public User getFriend() {
+        return friend;
+    }
+
+    public LocalDateTime getFriendsSince() {
+        return friendsSince;
+    }
+}
